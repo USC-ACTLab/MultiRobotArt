b787b9c2b41cd237134bbbc1f68ac775c65a9584
ewinge1 <ericewin@usc.edu>
2023-05-22 15:43:21 -0400

    Motion Python Commands

--- blockly-scripts/python_translations/blockTranslations.py
+++ blockly-scripts/python_translations/blockTranslations.py
@@ -1,15 +1,48 @@
 import numpy as np
 
-# Motion Primitive Commands
+###
+#  Motion Primitive Commands
+###
 
-def goto_at_speed(cf, x, y, z, v):
+def goto_at_speed(cf, x, y, z, v, rel=False):
     curr_pos = cf.getPosition()
     dist = np.linalg.norm(curr_pos, np.array([x, y, z]))
     duration = dist / v
-    cf.goTo(x, y, z, duration=duration)
+    cf.goTo(x, y, z, duration=duration, rel=rel)
 
-def goto_duration(cf, x, y, z, duration):
-    cf.goTo(x, y, z, duration=duration)
+def goto_duration(cf, x, y, z, duration, rel=False):
+    cf.goTo(x, y, z, duration=duration, rel=rel)
 
+def goto_rel_at_speed(cf, x, y, z, v):
+    goto_at_speed(cf, x, y, z, v, True)
 
-# Trajectory commands...
+def goto_rel_duration(cf, x, y, z, duration):
+    goto_duration(cf, x, y, z, duration, True)
+
+#TODO Check on cf.getPosition()
+def stop_and_hover(cf, height=None):
+    position = cf.getPosition()
+    goal_pos = position
+    if height != None:
+        goal_pos[2] = height
+    cf.goTo(goal_pos, duration=2.0)
+
+###
+#  Low Level Commands...
+###
+def getPosition(cf):
+    return cf.getPosition()
+
+#TODO: Enable low level commands...
+def cmdPos(cf, pos):
+    cf.cmdPos(pos)
+
+def enableHighLevelCommander(cf):
+    cf.notifySetpointsStop()
+
+### 
+#  Trajectory commands...
+###
+
+def circle():
+    pass
