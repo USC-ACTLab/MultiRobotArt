ee12b2c62887d7b658c07fe9ce72320cda7340e3
ewinge1 <ericewin@usc.edu>
2023-05-24 17:57:50 -0400

    Added simulator commands

--- mra-control/src/state/simulatorCommands.ts
+++ mra-control/src/state/simulatorCommands.ts
@@ -13,11 +13,77 @@ export interface SimulatorGroupState {
   robotIDs: string[];
 }
 
-export const go_to_xyz = (groupState: SimulatorGroupState, x: number, y: number, z: number, speed: number) => {
+export const go_to_xyz_speed = (groupState: SimulatorGroupState, x: number, y: number, z: number, speed: number) => {
   groupState.robotIDs.forEach((robotId) => {
+    var curr_position
+    if (useSimulator.getState().robots[robotId] == undefined){
+      curr_position = new Vector3(0, 0, 0)
+    }
+    else{
+      curr_position = useSimulator.getState().robots[robotId].pos;
+    }
     const newTrajectory = useSimulator.getState().robotGoTo(robotId, new Vector3(x, y, z), new Vector3(0, 0, 0), new Vector3(0, 0, 0));
-    useSimulator.getState().updateTrajectory(robotId, newTrajectory, speed);
+    const duration = curr_position.distanceTo(new Vector3(x, y, z)) / speed;
+    useSimulator.getState().updateTrajectory(robotId, newTrajectory, duration);
   });
 };
+
+export const go_to_xyz_duration = (groupState: SimulatorGroupState, x: number, y: number, z: number, duration: number) =>{
+  groupState.robotIDs.forEach((robotId) => {
+    const newTrajectory = useSimulator.getState().robotGoTo(robotId, new Vector3(x, y, z), new Vector3(0, 0, 0), new Vector3(0, 0, 0));
+    useSimulator.getState().updateTrajectory(robotId, newTrajectory, duration)
+  })
+}
+
+export const land = (groupState: SimulatorGroupState, height: number, duration: number) => {
+  groupState.robotIDs.forEach((robotId) => {
+    var curr_position
+    if (useSimulator.getState().robots[robotId] == undefined){
+      curr_position = new Vector3(0, 0, 0)
+    }
+    else{
+      curr_position = useSimulator.getState().robots[robotId].pos;
+    }    var goal_position = curr_position
+    goal_position.z = height
+    const newTrajectory = useSimulator.getState().robotGoTo(robotId, goal_position, new Vector3(0, 0, 0), new Vector3(0, 0, 0));
+    useSimulator.getState().updateTrajectory(robotId, newTrajectory, duration)
+  })
+}
+
+export const takeoff = (groupState: SimulatorGroupState, height: number, duration: number) => {
+  groupState.robotIDs.forEach((robotId) => {
+    var curr_position
+    if (useSimulator.getState().robots[robotId] == undefined){
+      curr_position = new Vector3(0, 0, 0)
+    }
+    else{
+      curr_position = useSimulator.getState().robots[robotId].pos;
+    }
+    console.log("Takeoff") ; 
+    console.log(robotId)  
+    var goal_position = curr_position;
+    goal_position.z = height;
+    const newTrajectory = useSimulator.getState().robotGoTo(robotId, goal_position, new Vector3(0, 0, 0), new Vector3(0, 0, 0));
+    useSimulator.getState().updateTrajectory(robotId, newTrajectory, duration)
+  })
+}
+
+export const move_speed = (groupState: SimulatorGroupState, x: number, y: number, z: number, speed: number) =>{
+  groupState.robotIDs.forEach((robotId) => {
+    var curr_position
+    if (useSimulator.getState().robots[robotId] == undefined){
+      curr_position = new Vector3(0, 0, 0)
+    }
+    else{
+      curr_position = useSimulator.getState().robots[robotId].pos;
+    }    var goal_position = curr_position;
+    goal_position.x += x;
+    goal_position.y += y;
+    goal_position.z += z;
+    const newTrajectory = useSimulator.getState().robotGoTo(robotId, goal_position, new Vector3(0, 0, 0), new Vector3(0, 0, 0));
+    var duration = new Vector3(x, y, z).length() / speed; 
+    useSimulator.getState().updateTrajectory(robotId, newTrajectory, duration)
+  })
+}
 
 export const dummy = () => {};
