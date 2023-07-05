c6b95739742b4d420ba0527911c2baaa26317e1b
Hammad Izhar <hammad_izhar@brown.edu>
2023-07-05 14:05:45 -0400

    render box3

--- mra-control/src/layout/simulation/Simulation.tsx
+++ mra-control/src/layout/simulation/Simulation.tsx
@@ -8,7 +8,7 @@ import { Group } from 'three';
 import * as THREE from 'three';
 
 export const Simulation = () => {
-  const marker = useRef<Group>(null!); 
+  const marker = useRef<Group>(null!);
   const robots = useSimulator((state) => state.robots);
   const updateTrajectory = useSimulator((state) => state.updateTrajectory);
   const robotGoTo = useSimulator((state) => state.robotGoTo);
@@ -25,8 +25,8 @@ export const Simulation = () => {
 
   return (
     <>
-     <color attach="background" args={['black']} />
-      <OrbitControls maxPolarAngle={Math.PI * (1/2 - 1 / 20)} minPolarAngle={0} minDistance={5} maxDistance={20} />
+      <color attach="background" args={['black']} />
+      <OrbitControls maxPolarAngle={Math.PI * (1 / 2 - 1 / 20)} minPolarAngle={0} minDistance={5} maxDistance={20} />
       <GizmoHelper alignment="bottom-right" margin={[80, 80]}>
         <GizmoViewport axisColors={['#9d4b4b', '#2f7f4f', '#3b5b9d']} labelColor="white" />
       </GizmoHelper>
@@ -40,17 +40,13 @@ export const Simulation = () => {
         fadeStrength={1.2}
         infiniteGrid={true}
       />
-      <Plane args={[1000, 1000]} rotation={[-Math.PI/2, 0, 0]} position={[0,-0.02, 0]}>
+      <Plane args={[1000, 1000]} rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.02, 0]}>
         <meshStandardMaterial color="black" />
       </Plane>
       {Object.values(robots).map((robot) => (
         <group key={robot.id} ref={marker} position={robot.pos}>
+          <pointLight position={[0, 0, 0]} intensity={1} color="white" />
           <Crazyflie />
-          <Sphere position={[0,-0.05,0]} scale={0.1} castShadow={false} receiveShadow={false}>
-            <meshDistanceMaterial />
-            <pointLight position={[0,0,0]} intensity={1} color="white"/>
-          </Sphere>
-          
         </group>
       ))}
     </>
