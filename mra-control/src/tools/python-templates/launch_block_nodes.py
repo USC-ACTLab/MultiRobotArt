6307fd9e8bf79994ae184feeb25a54804e9e9afc
Eric-Ewing <43459843+Eric-Ewing@users.noreply.github.com>
2024-09-23 22:48:05 -0400

    Update launch_block_nodes.py

--- mra-control/src/tools/python-templates/launch_block_nodes.py
+++ mra-control/src/tools/python-templates/launch_block_nodes.py
@@ -3,7 +3,20 @@ from ament_index_python.packages import get_package_share_directory
 from crazyflie_py import Crazyswarm
 import rclpy
 import threading
+from rclpy.node import Node
+class crazyflie_node(Node):
+    def __init__(self, crazyswarm):
+        super().__init__("crazyswarm_node")
+        Hz = 20
+        self.crazyswarm = crazyswarm
+        self.timeHelper = crazyswarm.timeHelper
+        self.timer = self.create_timer(
+            1 / Hz,
+            self.timer_callback,
+        )
 
+    def timer_callback(self):
+        self.timeHelper.sleepForRate(50)
 # Inject Imports Here:
 
 def launch(nodes):
@@ -39,7 +52,7 @@ def main():
 
     #   -----------Insert Nodes Here----------- 
 
-    
+    nodes.append(crazyflie_node(swarm))
     # Launch all nodes
     return launch(nodes)
 
